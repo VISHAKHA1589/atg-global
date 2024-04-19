0dfed30463b3afeed0f06fa1887c81ad2c1fda3c
VISHAKHA1589 <bishakham3@gmail.com>
2024-04-19 20:08:37 +0530

    first commit

---
+++ src/components/FeedPage.jsx
@@ -0,0 +1,236 @@
+import React from "react";
+
+const FeedPage = () => {
+  return (
+    <div className="feed-page">
+      <div class="container  ">
+        <div class="d-flex justify-content-center row">
+          <div class="col-md-8">
+            <div class="feed p-2">
+              <div>
+                <div class="bg-white border mt-2 feed-image1">
+                  <div class="d-flex justify-content-end socials p-2 py-3">
+                    <i class="fa fa-thumbs-up"></i>
+                    <i class="fa fa-comments-o"></i>
+                    <i class="fa fa-share"></i>
+                  </div>
+                </div>
+
+                <div className="bg-white border  feed-text">
+                  <p className="article">✍️ Article</p>
+                  <div className="d-flex">
+                    <p className="article-text ">
+                      What if famous brands had regular fonts? Meet
+                      RegulaBrands!
+                    </p>
+                    <svg
+                      width="28"
+                      height="28"
+                      viewBox="0 0 28 28"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="menu-button"
+                    >
+                      <path
+                        d="M18.6667 14C18.6667 15.2833 19.7167 16.3333 21 16.3333C22.2834 16.3333 23.3334 15.2833 23.3334 14C23.3334 12.7166 22.2834 11.6666 21 11.6666C19.7167 11.6666 18.6667 12.7166 18.6667 14ZM16.3334 14C16.3334 12.7166 15.2834 11.6666 14 11.6666C12.7167 11.6666 11.6667 12.7166 11.6667 14C11.6667 15.2833 12.7167 16.3333 14 16.3333C15.2834 16.3333 16.3334 15.2833 16.3334 14ZM9.33337 14C9.33337 12.7166 8.28337 11.6666 7.00004 11.6666C5.71671 11.6666 4.66671 12.7166 4.66671 14C4.66671 15.2833 5.71671 16.3333 7.00004 16.3333C8.28337 16.3333 9.33337 15.2833 9.33337 14Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </div>
+                  <div>
+                    <p className="post-description">
+                      I’ve worked in UX for the better part of a decade. From
+                      now on, I plan to rei…
+                    </p>
+                  </div>
+                  <div className="d-flex">
+                    <img
+                      className="user-picture"
+                      src="https://s3-alpha-sig.figma.com/img/d809/8d9d/f42d73f4b2b7835b4c918c4edcf10f64?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jsmG-ZoUnlON3A3ZbCB54sPIcnH~vyiGeh1IOmgUJhP6TxommJ4NxovduM-KmaE~v8YgV-2i-tMsx-pWcaOEGV-o2JkwSw~YW4JVsdiK65gPyGdICoP4KbhzKHMF8U60NVYgOV7r3TY8FQAieJRHysAAuOxBE3HMY40SfuBU21b95zbiQtXRtF1kg4~lvaeE5oaPyEfP6Nk7D3d90N2pbBN6rPTcl7xF4CjHTN8lNGwaY~YwLVfCgQdbyITZiXyzj2Fph3Sfsyj3n4JZlyZrB~29krPtfO6bYQWCWT2jbj5T9bIlHdME-cHh7zs3SnX~P4Ipd~gCd39-6Ndg5Pf2ag__"
+                      alt="user "
+                    />
+                    <p className="userName">Sarthak Kamra</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container ">
+        <div class="d-flex justify-content-center row">
+          <div class="col-md-8">
+            <div class="feed p-2">
+              <div>
+                <div class="bg-white border mt-2 feed-image2"></div>
+
+                <div className="bg-white border  feed-text">
+                  <p className="article">🔬️ Education</p>
+                  <div className="d-flex">
+                    <p className="article-text ">
+                      Tax Benefits for Investment under National Pension Scheme
+                      launched by Government
+                    </p>
+                    <svg
+                      width="28"
+                      height="28"
+                      viewBox="0 0 28 28"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="menu-button"
+                    >
+                      <path
+                        d="M18.6667 14C18.6667 15.2833 19.7167 16.3333 21 16.3333C22.2834 16.3333 23.3334 15.2833 23.3334 14C23.3334 12.7166 22.2834 11.6666 21 11.6666C19.7167 11.6666 18.6667 12.7166 18.6667 14ZM16.3334 14C16.3334 12.7166 15.2834 11.6666 14 11.6666C12.7167 11.6666 11.6667 12.7166 11.6667 14C11.6667 15.2833 12.7167 16.3333 14 16.3333C15.2834 16.3333 16.3334 15.2833 16.3334 14ZM9.33337 14C9.33337 12.7166 8.28337 11.6666 7.00004 11.6666C5.71671 11.6666 4.66671 12.7166 4.66671 14C4.66671 15.2833 5.71671 16.3333 7.00004 16.3333C8.28337 16.3333 9.33337 15.2833 9.33337 14Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </div>
+                  <div>
+                    <p className="post-description">
+                      I’ve worked in UX for the better part of a decade. From
+                      now on, I plan to rei…
+                    </p>
+                  </div>
+                  <div className="d-flex">
+                    <img
+                      className="user-picture"
+                      src="https://s3-alpha-sig.figma.com/img/b8a9/097f/c3ac7682e52df2e10d0a36ec3f243a44?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cdTiifG6krVdTB9~DOM~cdE4poKGxg7Y70apDK9-qsw1jZxmD0uFSH6Xm0lE1w~LL-N41QTnjPMNFxvYLa4KMdh7jBOXgMS-ebG2oQL5uOvjxyOxWgLcCp7d1NurGxx66T8WL6IzW4-1KqDzZMaFRcN8WVyUejay7boZ8Thqqbx2X4P2pPi1Qimhr34pRdqnj9lAHI59xRpWHBRcwyGaJrJ77NZHp92Fb5xouerxR3QSHnOF2vnDMYPk4LuDvpp7HuOptPJGCvZ6AHy3ytGZ7qCcPQh9-KstRDst9NG8p7q9Yf3f6ZzvLTXJ5srpRYpPOEcrbth2eyvL~QxPR1vmJQ__"
+                      alt="user "
+                    />
+                    <p className="userName">Ronal Jones</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container ">
+        <div class="d-flex justify-content-center row">
+          <div class="col-md-8">
+            <div class="feed p-2">
+              <div>
+                <div className="bg-white border  feed-text">
+                  <p className="article">🔬️ Education</p>
+                  <div className="d-flex">
+                    <p className="article-text ">
+                      Tax Benefits for Investment under National Pension Scheme
+                      launched by Government
+                    </p>
+                    <svg
+                      width="28"
+                      height="28"
+                      viewBox="0 0 28 28"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="menu-button"
+                    >
+                      <path
+                        d="M18.6667 14C18.6667 15.2833 19.7167 16.3333 21 16.3333C22.2834 16.3333 23.3334 15.2833 23.3334 14C23.3334 12.7166 22.2834 11.6666 21 11.6666C19.7167 11.6666 18.6667 12.7166 18.6667 14ZM16.3334 14C16.3334 12.7166 15.2834 11.6666 14 11.6666C12.7167 11.6666 11.6667 12.7166 11.6667 14C11.6667 15.2833 12.7167 16.3333 14 16.3333C15.2834 16.3333 16.3334 15.2833 16.3334 14ZM9.33337 14C9.33337 12.7166 8.28337 11.6666 7.00004 11.6666C5.71671 11.6666 4.66671 12.7166 4.66671 14C4.66671 15.2833 5.71671 16.3333 7.00004 16.3333C8.28337 16.3333 9.33337 15.2833 9.33337 14Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </div>
+                  <div>
+                    <p className="post-description">
+                      I’ve worked in UX for the better part of a decade. From
+                      now on, I plan to rei…
+                    </p>
+                  </div>
+                  <div className="d-flex">
+                    <img
+                      className="user-picture"
+                      src="https://s3-alpha-sig.figma.com/img/bdc0/79a9/a2e530610a1b91c9682ba7889c87c1b2?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=B785~FxUO9Lc3ink4Vuk4SZMLMuGywzaeNjCqFiCj9W1L~Dn1G2F9KzhghjnZJMUnNkWdJQxeB9kjF5vAVMxdECGiGUHahhkRdgPsNNuBF4DpzQd~6FORdFL48V5rpG2vv~sCWH~iSrf8PzQiobuPnZn~caCdNw1C7H~3p5Fpln91ex7w3Ze2wFbAY6pNthjrhJHDzov0HKsO~RGidq6qJYfZIKtXaIaUmjOTPqzLDCt0fpdidL0yTMwMVdUJDIVr4uNjBGvbOiNDQJ33EGv1-7FzCgaVm-BLk8E5Yy~Wi57PxMtckjZm9hpZFcXBh9ZgObZpbX7~-hFhgc0LFt9OA__"
+                      alt="user "
+                    />
+                    <p className="userName">Ronal Jones</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="feed-side flex">
+        <div className="d-flex">
+          <div className="location">
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clip-path="url(#clip0_1_842)">
+                <path
+                  d="M9 1.5C6.0975 1.5 3.75 3.8475 3.75 6.75C3.75 10.6875 9 16.5 9 16.5C9 16.5 14.25 10.6875 14.25 6.75C14.25 3.8475 11.9025 1.5 9 1.5ZM5.25 6.75C5.25 4.68 6.93 3 9 3C11.07 3 12.75 4.68 12.75 6.75C12.75 8.91 10.59 12.1425 9 14.16C7.44 12.1575 5.25 8.8875 5.25 6.75Z"
+                  fill="black"
+                />
+                <path
+                  d="M9 8.625C10.0355 8.625 10.875 7.78553 10.875 6.75C10.875 5.71447 10.0355 4.875 9 4.875C7.96447 4.875 7.125 5.71447 7.125 6.75C7.125 7.78553 7.96447 8.625 9 8.625Z"
+                  fill="black"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_1_842">
+                  <rect width="18" height="18" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+            <p>Noida, India</p>
+          </div>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="edit"
+          >
+            <g clip-path="url(#clip0_1_848)">
+              <path
+                d="M2.5 14.375V17.5H5.625L14.8417 8.28334L11.7167 5.15834L2.5 14.375ZM17.2583 5.86667C17.5833 5.54167 17.5833 5.01667 17.2583 4.69167L15.3083 2.74167C14.9833 2.41667 14.4583 2.41667 14.1333 2.74167L12.6083 4.26667L15.7333 7.39167L17.2583 5.86667Z"
+                fill="black"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1_848">
+                <rect width="20" height="20" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+
+        <hr className="feed-side-line" />
+      </div>
+      <div className="info">
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="info-text"
+        >
+          <g opacity="0.5" clip-path="url(#clip0_1_853)">
+            <path
+              d="M7.33337 10H8.66671V11.3334H7.33337V10ZM7.33337 4.66671H8.66671V8.66671H7.33337V4.66671ZM7.99337 1.33337C4.31337 1.33337 1.33337 4.32004 1.33337 8.00004C1.33337 11.68 4.31337 14.6667 7.99337 14.6667C11.68 14.6667 14.6667 11.68 14.6667 8.00004C14.6667 4.32004 11.68 1.33337 7.99337 1.33337ZM8.00004 13.3334C5.05337 13.3334 2.66671 10.9467 2.66671 8.00004C2.66671 5.05337 5.05337 2.66671 8.00004 2.66671C10.9467 2.66671 13.3334 5.05337 13.3334 8.00004C13.3334 10.9467 10.9467 13.3334 8.00004 13.3334Z"
+              fill="black"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_1_853">
+              <rect width="16" height="16" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+        <p>
+          Your location will help us serve better <br />
+          and extend a personalised experience.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FeedPage;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+     <div className='hero-image '><span className='hero-text'>Computer Engineering</span><span className='hero-text-bottom'>142,765 Computer Engineers follow this</span></div>
+      </div>
+  )
+}
+
+export default Hero
